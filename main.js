078d45c7658b133a7ea7a2c86632abf2dc88186f
nzahoualoojeankc-lab <nzahoualoojeankc@gmail.com>
2026-09-17 21:20:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,23 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./service-worker.js')
       .then((registration) => {
         console.log('Service Worker enregistré avec succès ! Portée :', registration.scope);
+
+        // Détecte si une mise à jour du code est trouvée sur GitHub
+        registration.addEventListener('updatefound', () => {
+          const newWorker = registration.installing;
+          
+          if (newWorker) {
+            newWorker.addEventListener('statechange', () => {
+              // Dès que le nouveau Service Worker est entièrement téléchargé et prêt
+              if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+                console.log('Nouvelle version détectée ! Application des modifications de GitHub...');
+                // Recharge automatiquement la page de l'utilisateur pour appliquer la mise à jour
+                window.location.reload();
+              }
+            });
+          }
+        });
+
       })
       .catch((error) => {
         console.warn('L\'enregistrement du Service Worker a échoué :', error);
